a2136c2390104b4a7bd500082e73f0c4532ed614
yunpeng.zhan <136688059@qq.com>
2022-01-13 19:34:50 +0800

    fix:unexported value

--- parser/parse.go
+++ parser/parse.go
@@ -16,6 +16,32 @@ const (
 	VALUE  = 70
 )
 
+type Row struct {
+	key       string
+	value     string
+	parent    *Row
+	childs    []*Row
+	parentKey string // parent.childs[middle] == this
+}
+
+func (r *Row) format() {
+	if r.parent == nil {
+		formatHead()
+	}
+	if r.childs != nil {
+		formatSideLine()
+		r.childs[len(r.childs)/2].parentKey = r.key
+		for _, v := range r.childs {
+			v.format()
+		}
+	} else {
+		formatLine(r.parentKey, r.key, r.value)
+	}
+	if r.parent == nil {
+		formatSideLine()
+	}
+}
+
 func formatHead() {
 	fmt.Printf("+%s+%s+%s+\n", strings.Repeat("-", PARENT), strings.Repeat("-", KEY), strings.Repeat("-", VALUE))
 	fmt.Printf("|%s|%s|%s|\n", "ROOT"+strings.Repeat(" ", PARENT-len("ROOT")), "KEY"+strings.Repeat(" ", KEY-len("KEY")), "VALUE"+strings.Repeat(" ", VALUE-len("VALUE")))
@@ -39,56 +65,46 @@ func formatLine(Parent string, Key, Value string) {
 
 // Print the struct to os.stdout
 // TODO: support output type
-func formatStructTable(t reflect.Type, v reflect.Value, Parent string, deepth int) {
-	defer func() {
-		if deepth == 0 {
-			formatSideLine()
-		}
-	}()
-	if deepth == 0 {
-		formatHead()
-	}
-
-	formatSideLine()
+func parseStructTable(t reflect.Type, v reflect.Value, Parent *Row) {
 	for i := 0; i < t.NumField(); i++ {
-		if t.Field(i).Type.Kind() == reflect.Struct && deepth == 0 {
-			formatStructTable(t.Field(i).Type, v.Field(i), t.Field(i).Name, 1)
+		var child = Row{parent: Parent}
+		child.key = t.Field(i).Name
+		Parent.childs = append(Parent.childs, &child)
+		if t.Field(i).Type.Kind() == reflect.Struct && Parent.parent == nil {
+			parseStructTable(t.Field(i).Type, v.Field(i), &child)
 			continue
 		}
-		var key, value string
-		key = t.Field(i).Name
-
+		if !v.Field(i).CanInterface() {
+			child.value = "<hidden field>"
+			continue
+		}
 		switch v.Field(i).Interface().(type) {
 		case string:
-			value, _ = v.Field(i).Interface().(string)
+			child.value, _ = v.Field(i).Interface().(string)
 		case int:
 			ret, _ := v.Field(i).Interface().(int)
-			value = strconv.Itoa(ret)
+			child.value = strconv.Itoa(ret)
 		case bool:
 			ret, _ := v.Field(i).Interface().(bool)
-			value = strconv.FormatBool(ret)
+			child.value = strconv.FormatBool(ret)
 		default:
-			value = fmt.Sprintf("%v", v.Field(i).Interface())
+			child.value = fmt.Sprintf("%v", v.Field(i).Interface())
 		}
 		re := regexp.MustCompile(`\r|\n|\t|[\r\n\v\f\x{0085}\x{2028}\x{2029}]`)
-		value = re.ReplaceAllString(value, "")
-		if value != "" {
-			if i == (t.NumField()-1)/2 {
-				formatLine(Parent, key, value)
-			} else {
-				formatLine("", key, value)
-			}
-		}
+		child.value = re.ReplaceAllString(child.value, "")
+
 	}
 }
 
 // Table show be a struct.
 // if you want to use a struct pointer, you shold get the value first
 // *T > T
-func Parse(root interface{}) error {
-	if reflect.TypeOf(root).Kind() != reflect.Struct {
+func Parse(ins interface{}) error {
+	if reflect.TypeOf(ins).Kind() != reflect.Struct {
 		return errors.New("root is not struct")
 	}
-	formatStructTable(reflect.TypeOf(root), reflect.ValueOf(root), "", 0)
+	var root = &Row{}
+	parseStructTable(reflect.TypeOf(ins), reflect.ValueOf(ins), root)
+	root.format()
 	return nil
 }
